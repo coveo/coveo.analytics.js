5c4e9206b40b50ad7445095ae707f33d10bd9418
Nicholas-David Labarre <ndlabarre@coveo.com>
2021-03-25 12:31:51 -0400

    [COM-814] support s3 location + add certifier

--- cypress/tests/VisitOptions.ts
+++ cypress/tests/VisitOptions.ts
@@ -3,11 +3,13 @@ export const getQueryParameters = () => {
         version: getVersion(),
         endpoint: getEndpoint(),
         token: getToken(),
+        s3Path: getS3Path(),
     };
 };
 
 const getToken = () => (Cypress.env('token') as string) || 'invalid';
 const getVersion = () => (Cypress.env('version') as string) || '2';
 const getEndpoint = () => (Cypress.env('endpoint') as string) || 'https://platform.cloud.coveo.com/rest/ua';
+const getS3Path = () => (Cypress.env('s3_path') as string) || '';
 
 export const getCollectEndpoint = () => `${getEndpoint()}/v15/analytics/collect`;

ca16bd79b9740ec6991725d6ce37db8aa05ee3f4
Luc Bergeron <10946843+lbergeron@users.noreply.github.com>
2021-03-30 16:58:19 -0400

    feat: expose handleOneAnalyticsEvent to headless

--- src/coveoua/headless.ts
+++ src/coveoua/headless.ts
@@ -1,4 +1,5 @@
 export {CoveoSearchPageClient, SearchPageClientProvider} from '../searchPage/searchPageClient';
 export {CoveoAnalyticsClient, AnalyticsClientSendEventHook} from '../client/analytics';
 export {PreprocessAnalyticsRequest} from '../client/analyticsRequestClient';
+export {handleOneAnalyticsEvent} from './simpleanalytics';
 export * as history from '../history';

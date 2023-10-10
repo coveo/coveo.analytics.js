7525e73df4de9df116d149280713b45b35528981
mmitiche <mmitiche@coveo.com>
2023-10-10 14:43:40 -0400

    getGeneratedAnswerMetadata function added to the InsightClientProvider

--- src/insight/insightClient.spec.ts
+++ src/insight/insightClient.spec.ts
@@ -63,6 +63,7 @@ describe('InsightClient', () => {
         getLanguage: () => 'en',
         getFacetState: () => fakeFacetState,
         getIsAnonymous: () => false,
+        getGeneratedAnswerMetadata: () => ({genratedAnswerMetadata: 'foo'}),
     };
 
     const initClient = () => {
@@ -78,7 +79,7 @@ describe('InsightClient', () => {
 
     const expectMatchPayload = (actionCause: SearchPageEvents | InsightEvents, meta = {}) => {
         const body: string = lastCallBody(fetchMock);
-        const customData = {foo: 'bar', ...meta};
+        const customData = {foo: 'bar', genratedAnswerMetadata: 'foo', ...meta};
         expect(JSON.parse(body)).toMatchObject({
             queryText: 'queryText',
             responseTime: 123,

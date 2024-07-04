921264297ecc64d81d3ce9684d28b1c97735adfe
Nicolas Legros <nlegros@coveo.com>
2024-07-04 11:51:29 -0400

    feat: Added `DRY_RUN` variable [UA-8891]

--- scripts/trigger-cd.mjs
+++ scripts/trigger-cd.mjs
@@ -6,7 +6,8 @@ await octokit.rest.repos.createDispatchEvent({
   event_type: 'deploy',
   client_payload: {
     run_Id: context.runId,
-    FORCE_DEPLOY: process.env.FORCE_DEPLOY
+    FORCE_DEPLOY: process.env.FORCE_DEPLOY,
+    DRY_RUN: process.env.DRY_RUN
   },
   owner: 'coveo-platform',
   repo: 'coveo.analytics.js-infra',
